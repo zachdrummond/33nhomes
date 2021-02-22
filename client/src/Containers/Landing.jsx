d9ec1eaf74aaf69e0b8e3b4f258c7f532c58a1df
Zach Drummond <zachdrummond3@gmail.com>
2021-02-22 14:17:25 -0500

    Added carousel with two pictures

--- client/src/Containers/Landing.jsx
+++ client/src/Containers/Landing.jsx
@@ -1,9 +1,13 @@
 import Navbar from "../Components/Navbar/Navbar";
+import Carousel from "../Components/Carousel/Carousel";
 
 const Landing = () => {
-    return (
-        <Navbar/>
-    );
+  return (
+    <>
+      <Navbar />
+      <Carousel />
+    </>
+  );
 };
 
 export default Landing;

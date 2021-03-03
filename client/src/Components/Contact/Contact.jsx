e2b49c329ea66d442c043bd90b34b62f7998dbbe
Zach Drummond <zachdrummond3@gmail.com>
2021-03-03 14:38:49 -0500

    Added a form to Contact

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <section className="my-5">
+      <div class="form-floating mb-3">
+        <input
+          type="text"
+          class="form-control"
+          id="floatingInput"
+          placeholder="First Name Last Name"
+        />
+        <label for="floatingInput">First and Last Name</label>
+      </div>
+      <div class="form-floating">
+        <input
+          type="text"
+          class="form-control"
+          id="floatingPassword"
+          placeholder="Last Name"
+        />
+        <label for="floatingPassword">Last Name</label>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;

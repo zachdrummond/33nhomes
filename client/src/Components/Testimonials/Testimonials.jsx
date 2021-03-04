08c263764dabfba6fcd5519a0f86d74eff3a4e0f
Zach Drummond <zachdrummond3@gmail.com>
2021-03-04 12:37:43 -0500

    Added cards for testimonials

--- client/src/Components/Testimonials/Testimonials.jsx
+++ client/src/Components/Testimonials/Testimonials.jsx
@@ -1,11 +1,58 @@
-import React from 'react';
-
 const Testimonials = () => {
-    return (
-        <div>
-            
+  return (
+    <section className="my-5">
+      <h2 className="text-center mb-4">Customer Reviews</h2>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-body">
+              <p class="card-text">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <h5 class="card-title">Card title</h5>
+            </div>
+          </div>
         </div>
-    );
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-body">
+              <p class="card-text">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <h5 class="card-title">Card title</h5>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-body">
+              <p class="card-text">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content.
+              </p>
+              <h5 class="card-title">Card title</h5>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <div class="card-body">
+              <p class="card-text">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <h5 class="card-title">Card title</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
